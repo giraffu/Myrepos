cc17b3417e0adbf71eb61b47dfd179593a389c0f
giraffu <cv1347968277@gmail.com>
2022-09-17 16:09:18 +0800

    创建

---
+++ MyRepos/HandleObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRepos
+{
+    public class HandleObject
+    {
+        /// <summary>
+        /// 得到一个对象的属性值
+        /// </summary>
+        /// <param name="obj">对象</param>
+        /// <param name="name">属性名称</param>
+        /// <returns>属性值</returns>
+        public object GetPropertyValue(object obj, string name)
+        {
+            Type type = obj.GetType();
+            PropertyInfo item = type.GetProperty(name);
+
+            object x = item.GetValue(obj);
+            return x;
+        }
+    }
+}
